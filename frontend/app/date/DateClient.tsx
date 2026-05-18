1e69d51fc2a4f815e3e2d61576c97dc122260a98
jonghyeon99 <twjh99@naver.com>
2026-05-18 17:16:57 +0900

    Change Nav-Card Font Color

--- frontend/app/date/DateClient.tsx
+++ frontend/app/date/DateClient.tsx
@@ -85,7 +85,6 @@ export default function DateClient() {
                         cursor: "pointer",
                         color: "var(--sumi)",
                         width: 200,
-                        colorScheme: "light",
                     }}
                 />
                 <button
